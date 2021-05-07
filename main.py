92ca4685810cdbd6590ce185903a47de6fd9aa97
dominikp7 <dominikpi7@o2.pl>
2021-05-07 22:30:35 +0200

    update

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-
 from tkinter import *
-from tkinter import messagebox
 from tkinter import Tk
+import base64
 
 
-
+# tworzenie metod szyfrowania Cezara i deszyfrowania
 
 KLUCZ = 3
 
-
-def szyfruj(txt):
+def szyfruj_cezar(txt):
     zaszyfrowny = ""
     for i in range(len(txt)):
         if ord(txt[i]) > 122 - KLUCZ:
@@ -19,50 +17,97 @@ def szyfruj(txt):
     return zaszyfrowny
 
 
-"""def main(args):
-    tekst = input("Podaj ciąg do zaszyfrowania:n")
-    print("Ciąg zaszyfrowany:n, szyfruj(tekst))
-    return 0"""
+def deszyfruj_cezar(tekst):
+    odszyfrowany = ""
+    KLUCZM = KLUCZ % 26
+    for znak in tekst:
+        if (ord(znak) - KLUCZM < 97):
+            odszyfrowany += chr(ord(znak) - KLUCZM + 26)
+        else:
+            odszyfrowany += chr(ord(znak) - KLUCZM)
+    return odszyfrowany
+
+def szyfruj_base64(txt):
+    message = txt
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
 
 
+def deszyfruj_base64(txt):
+    base64_message = txt
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    return message
+
+
+# tworzenie okna aplikacji i ustawienie jego domyslnych wymiarow
 
 okno = Tk()
 okno.geometry("300x300")
 
 
 
+# tworzenie podzialu
 
 topFrame = Frame(okno)
 topFrame.pack()
 bottomFrame = Frame(okno)
 bottomFrame.pack(side = BOTTOM)
 
-entry1 = Entry(okno)
-entry1.pack(side = TOP)
 
-def labelka():
-    mylable = Label(okno, text=szyfruj(entry1.get()))
-    mylable.pack()
+# tworzenie okna do wprowadzania danych
 
+wprowadz_tekst = Entry(okno, width = 100)
+wprowadz_tekst.pack(side = TOP)
+
+
+# inicjowanie
+
+class Szyfruj:
+    def __init__(self):
+        pass
+
+    def et_szyfr_cezar(self):
+        et_szyfrowanie = Label(okno, text = szyfruj_cezar(wprowadz_tekst.get()))
+        self.pozyskany_tekst = szyfruj_cezar(wprowadz_tekst.get())
+        et_szyfrowanie.pack()
+
+    def et_deszyfr_cezar(self):
+        et_szyfrowanie = Label(okno, text = deszyfruj_cezar(self.pozyskany_tekst))
+        et_szyfrowanie.pack()
+
+
+    def et_szyfr_base64(self):
+        et_szyfrowanie = Label(okno, text = szyfruj_base64(wprowadz_tekst.get()))
+        self.pozyskany_tekst2 = szyfruj_base64(wprowadz_tekst.get())
+        et_szyfrowanie.pack()
+
+    def et_deszyfr_base64(self):
+        et_szyfrowanie = Label(okno, text = deszyfruj_base64(self.pozyskany_tekst2))
+        et_szyfrowanie.pack()
+
+
+# tworzenie przyciskow i przypisywanie do nich funkcji
 
 podaj_slowo = Label(topFrame, text = 'Podaj słowo')
 
+szyfr = Szyfruj()
+przycisk_sz_cezar = Button(bottomFrame, text = 'Szyfr Cezara', command = szyfr.et_szyfr_cezar)
+przycisk_desz_cezar = Button(bottomFrame, text = 'Deszyfrowanie Cezar', command = szyfr.et_deszyfr_cezar)
 
-przycisk1 = Button(bottomFrame, text = 'guzik', command = labelka)
-przycisk2 = Button(bottomFrame, text = 'test', command = szyfruj('morda'))
-
-
-
-
-podaj_slowo.pack(side = LEFT)
-
-przycisk2.pack(side = LEFT)
-przycisk1.pack(side = TOP)
+przycisk_sz_base64 = Button(bottomFrame, text = 'Szyfr base64', command = szyfr.et_szyfr_base64)
+przycisk_desz_base64 = Button(bottomFrame, text = 'Deszyfrowanie base64', command = szyfr.et_deszyfr_base64)
 
 
+podaj_slowo.pack(side = TOP)
 
+przycisk_sz_cezar.grid(row = 1, column = 1)
+przycisk_desz_cezar.grid(row = 1, column = 2)
 
+przycisk_sz_base64.grid(row = 2, column = 1)
+przycisk_desz_base64.grid(row = 2, column = 2)
 
 okno.mainloop()
-
-
